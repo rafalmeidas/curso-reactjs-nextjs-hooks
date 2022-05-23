f4f891c5f9e9af2f4c5063da130db6e8ea0e7f0b
Rafael Almeida Silva <rafalmeidas21@gmail.com>
2022-05-23 10:47:02 -0300

    Criando hook personalizado

--- src/App.jsx
+++ src/App.jsx
@@ -26,27 +26,9 @@ const useMyHook = (callback, delay = 1000) => {
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [delay, setDelay] = useState(1000);
-  const [incrementor, setIncrementor] = useState(100);
-
-  useMyHook(() => setCounter((c) => c + 1), delay);
-
   return (
     <div>
-      <h1>Contador: {counter}</h1>
-      <h1>Delay: {delay}</h1>
-      <button onClick={() => setDelay((d) => d + incrementor)}>
-        +{incrementor}
-      </button>
-      <button onClick={() => setDelay((d) => d - incrementor)}>
-        -{incrementor}
-      </button>
-      <input
-        type="number"
-        value={incrementor}
-        onChange={(e) => setIncrementor(Number(e.target.value))}
-      />
+      <h1>Oi</h1>
     </div>
   );
 }
---
+++ src/AppCreateHook.jsx
@@ -0,0 +1,54 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from 'react';
+import P from 'prop-types';
+import './App.css';
+
+const useMyHook = (callback, delay = 1000) => {
+  const savedCb = useRef();
+
+  useEffect(() => {
+    savedCb.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      savedCb.current();
+    }, delay);
+
+    return () => clearInterval(interval);
+  }, [delay]);
+};
+
+function AppCreateHook() {
+  const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [incrementor, setIncrementor] = useState(100);
+
+  useMyHook(() => setCounter((c) => c + 1), delay);
+
+  return (
+    <div>
+      <h1>Contador: {counter}</h1>
+      <h1>Delay: {delay}</h1>
+      <button onClick={() => setDelay((d) => d + incrementor)}>
+        +{incrementor}
+      </button>
+      <button onClick={() => setDelay((d) => d - incrementor)}>
+        -{incrementor}
+      </button>
+      <input
+        type="number"
+        value={incrementor}
+        onChange={(e) => setIncrementor(Number(e.target.value))}
+      />
+    </div>
+  );
+}
+
+export default AppCreateHook;
